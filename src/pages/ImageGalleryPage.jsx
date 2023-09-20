617ccaf3d23abdd180bd8e457d93012f2fda7601
thatkhay <johnkingsleyegeonu@gmail.com>
2023-09-20 21:50:04 +0100

    edit

--- src/pages/ImageGalleryPage.jsx
+++ src/pages/ImageGalleryPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
 import { Container, Button, TextField, AppBar, Toolbar, Typography } from '@mui/material';
@@ -17,27 +17,27 @@ import SummerThree from '../assets/summer3.jpg';
 import SpringThree from '../assets/spring3.jpg';
 
 const initialImages = [
-  { src: Fall, alt: 'Fall', tag: 'fall' },
-  { src: Winter, alt: 'Winter', tag: 'winter' },
-  { src: Summer, alt: 'Summer', tag: 'summer' },
-  { src: Spring, alt: 'Spring', tag: 'spring' },
-  { src: FallTwo, alt: 'Fall 2', tag: 'fall' },
-  { src: WinterTwo, alt: 'Winter 2', tag: 'winter' },
-  { src: SummerTwo, alt: 'Summer 2', tag: 'summer' },
-  { src: SpringTwo, alt: 'Spring 2', tag: 'spring' },
-  { src: FallThree, alt: 'Fall 3', tag: 'fall' },
-  { src: WinterThree, alt: 'Winter 3', tag: 'winter' },
-  { src: SummerThree, alt: 'Summer 3', tag: 'summer' },
-  { src: SpringThree, alt: 'Spring 3', tag: 'spring' },
+  { id: 'fall', src: Fall, alt: 'Fall', tag: 'fall' },
+  { id: 'winter', src: Winter, alt: 'Winter', tag: 'winter' },
+  { id: 'summer', src: Summer, alt: 'Summer', tag: 'summer' },
+  { id: 'spring', src: Spring, alt: 'Spring', tag: 'spring' },
+  { id: 'fall2', src: FallTwo, alt: 'Fall 2', tag: 'fall' },
+  { id: 'winter2', src: WinterTwo, alt: 'Winter 2', tag: 'winter' },
+  { id: 'summer2', src: SummerTwo, alt: 'Summer 2', tag: 'summer' },
+  { id: 'spring2', src: SpringTwo, alt: 'Spring 2', tag: 'spring' },
+  { id: 'fall3', src: FallThree, alt: 'Fall 3', tag: 'fall' },
+  { id: 'winter3', src: WinterThree, alt: 'Winter 3', tag: 'winter' },
+  { id: 'summer3', src: SummerThree, alt: 'Summer 3', tag: 'summer' },
+  { id: 'spring3', src: SpringThree, alt: 'Spring 3', tag: 'spring' },
 ];
 
-const Image = ({ src, alt, index, tag }) => {
+const Image = ({ src, alt, id, index, tag, handleDragEnd }) => {
   const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
-    id: `image-${index}`,
+    id,
   });
 
   const { setNodeRef: setDropRef } = useDroppable({
-    id: `image-${index}`,
+    id,
   });
 
   const style = {
@@ -95,36 +95,26 @@ const DraggableImageGrid = () => {
   const [images, setImages] = useState(initialImages);
   const [searchQuery, setSearchQuery] = useState('');
 
-  const shuffleImages = () => {
-    const shuffledImages = [...images];
-    for (let i = shuffledImages.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledImages[i], shuffledImages[j]] = [shuffledImages[j], shuffledImages[i]];
-    }
-    setImages(shuffledImages);
-  };
-
   const onDragEnd = ({ active, over }) => {
-    if (active.id !== over.id) {
-      const fromIndex = parseInt(active.id.split('-')[1]);
-      const toIndex = parseInt(over.id.split('-')[1]);
-
+    if (active && over && active.id !== over.id) {
+      const fromIndex = images.findIndex((img) => img.id === active.id);
+      const toIndex = images.findIndex((img) => img.id === over.id);
+      const movedImage = images[fromIndex];
       const updatedImages = [...images];
-      const [movedImage] = updatedImages.splice(fromIndex, 1);
+
+      updatedImages.splice(fromIndex, 1);
       updatedImages.splice(toIndex, 0, movedImage);
       setImages(updatedImages);
     }
   };
 
-  const filterImages = () => {
+  const filteredImages = useCallback(() => {
     if (!searchQuery) {
       return images;
     }
 
-    return images.filter((image) =>
-      image.alt.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-  };
+    return images.filter((image) => image.alt.toLowerCase().includes(searchQuery.toLowerCase()));
+  }, [images, searchQuery]);
 
   return (
     <DndContext onDragEnd={onDragEnd}>
@@ -142,10 +132,17 @@ const DraggableImageGrid = () => {
           </Toolbar>
         </AppBar>
         <Button
-          onClick={shuffleImages}
           variant="contained"
           color="primary"
           style={{ marginTop: '2rem' }}
+          onClick={() => {
+            const shuffledImages = [...images];
+            for (let i = shuffledImages.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              [shuffledImages[i], shuffledImages[j]] = [shuffledImages[j], shuffledImages[i]];
+            }
+            setImages(shuffledImages);
+          }}
         >
           Shuffle Images
         </Button>
@@ -174,13 +171,15 @@ const DraggableImageGrid = () => {
             borderRadius: '.6rem',
           }}
         >
-          {filterImages().map((image, index) => (
+          {filteredImages().map((image, index) => (
             <Image
               key={`image-${index}`}
               src={image.src}
               alt={image.alt}
+              id={image.id}
               tag={image.tag}
               index={index}
+              handleDragEnd={onDragEnd}
             />
           ))}
         </Container>
